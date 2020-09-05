bb2d6050e36003ac6b572438f1b6d6e6804f3ae1
iDrath <55280378+iDrath@users.noreply.github.com>
2020-09-05 00:17:10 -0400

    icon

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -7,6 +7,8 @@
 		<!-- Styles -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     	<link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" >
+		<link href="{{ asset('css/editedbootstrap.css') }}" rel="stylesheet" type="text/css" >
+
 
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -43,14 +45,13 @@
 
 
 		<nav class="navbar navbar-expand-lg navbar-light menunav">
-			<a class="navbar-brand" href="#">Navbar</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarNavDropdown">
 				<ul class="navbar-nav">
-				<li class="nav-item active">
-					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				<li class="nav-item">
+					<a class="nav-link" href="#"><span class="glyphicon glyphicon-home"></span></a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" href="#">Features</a>
