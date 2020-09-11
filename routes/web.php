dca0ba6db01e959a4102c5614db404265f1817d7
iDrath <55280378+iDrath@users.noreply.github.com>
2020-09-11 00:45:13 -0300

    controller

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', 'PagesController@index');
 
-Route::get('/posts', function () {
-    return view('posts');
-});
+Route::get('/posts', 'PagesController@posts');
